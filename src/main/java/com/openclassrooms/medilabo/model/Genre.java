d5f37ca590e533de9d4a154d174056d27b3352c6
atezier <atezier@outlook.fr>
2024-02-25 21:46:05 +0100

    Create User and Genre

---
+++ src/main/java/com/openclassrooms/medilabo/model/Genre.java
@@ -0,0 +1,5 @@
+package main.java.com.openclassrooms.medilabo.model;
+
+public enum Genre {
+    MALE, FEMALE, OTHER
+}
---
+++ src/main/java/com/openclassrooms/medilabo/model/User.java
@@ -0,0 +1,10 @@
+package main.java.com.openclassrooms.medilabo.model;
+
+public class User {
+    String firstName;
+    String lastName;
+    String date;
+    Genre genre;
+    String address;
+    String phone;
+}

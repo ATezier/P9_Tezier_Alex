62f967bd31c2b67b285cd1f0d0b72923d5a8f433
atezier <atezier@outlook.fr>
2024-03-11 17:11:23 +0100

    Rename url and add get reports section

--- microservice-patient/src/main/java/com/openclassrooms/microservicepatient/controller/PatientController.java
+++ microservice-patient/src/main/java/com/openclassrooms/microservicepatient/controller/PatientController.java
@@ -14,12 +14,12 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
-    @GetMapping("/patients")
+    @GetMapping("patient/list")
     public ResponseEntity<List<Patient>> getAllPatients() {
         return new ResponseEntity<>(patientService.findAllPatients(), HttpStatus.OK);
     }
 
-    @GetMapping("/patient")
+    @GetMapping("patient/get")
     public ResponseEntity<Patient> getPatientByFirstNameAndLastName(@RequestParam String firstName, @RequestParam String lastName) {
         ResponseEntity<Patient> res = null;
         try {
@@ -30,39 +30,50 @@ public class PatientController {
         return res;
     }
 
-    @PostMapping("/patient")
-    public ResponseEntity<?> createPerson(@RequestBody Patient patient) {
-        ResponseEntity<?> res = null;
+    @GetMapping("patient/get/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Long id) {
+        ResponseEntity<Patient> res = null;
         try {
-            patientService.createPatient(patient);
-            res = new ResponseEntity<>(HttpStatus.CREATED);
+            res = new ResponseEntity<>(patientService.findPatientById(id), HttpStatus.OK);
         } catch (IllegalArgumentException e) {
-            res = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            res = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return res;
     }
 
-    @PutMapping("/patient")
-    public ResponseEntity<?> updatePerson(@RequestParam String firstName, @RequestParam String lastName, @RequestBody Patient patient) {
+    @PostMapping("patient/create")
+    public ResponseEntity<?> createPerson(@RequestBody Patient patient) {
         ResponseEntity<?> res = null;
         try {
-            patientService.updatePatient(firstName, lastName, patient);
+            patientService.createPatient(patient);
+            res = ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException e) {
+            res = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        return res;
+    }
+
+    @PutMapping("patient/update/{id}")
+    public ResponseEntity<?> updatePerson(@PathVariable Long id, @RequestBody Patient patient) {
+        ResponseEntity<?> res = null;
+        try {
+            patientService.updatePatient(id, patient);
             res = new ResponseEntity<>(HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             if(e.getMessage().contains("Patient not found")) {
                 res = new ResponseEntity<>(HttpStatus.NOT_FOUND);
             } else {
-                res = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                res = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
             }
         }
         return res;
     }
 
-    @DeleteMapping("/patient")
-    public ResponseEntity<?> deletePerson(@RequestParam String firstName, @RequestParam String lastName) {
+    @DeleteMapping("patient/delete/{id}")
+    public ResponseEntity<?> deletePerson(@PathVariable Long id) {
         ResponseEntity<?> res = null;
         try {
-            patientService.deletePatient(firstName, lastName);
+            patientService.deletePatient(id);
             res = new ResponseEntity<>(HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             res = new ResponseEntity<>(HttpStatus.NOT_FOUND);

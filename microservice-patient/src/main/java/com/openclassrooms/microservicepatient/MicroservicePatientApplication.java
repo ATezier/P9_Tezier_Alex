88b82a164d525e74c5c4f1ada5e76e6e58e90fdc
atezier <atezier@outlook.fr>
2024-03-11 16:55:46 +0100

    remove unused dependencies

--- microservice-patient/src/main/java/com/openclassrooms/microservicepatient/MicroservicePatientApplication.java
+++ microservice-patient/src/main/java/com/openclassrooms/microservicepatient/MicroservicePatientApplication.java
@@ -2,6 +2,7 @@ package com.openclassrooms.microservicepatient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroservicePatientApplication {
